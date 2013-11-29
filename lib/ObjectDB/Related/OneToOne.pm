4bcd2556fe27a85347304c13bc98ecd5554c28a9
vti <viacheslav.t@gmail.com>
2013-11-29 22:21:51 +0200

    cleanup

--- lib/ObjectDB/Related/OneToOne.pm
+++ lib/ObjectDB/Related/OneToOne.pm
@@ -23,57 +23,43 @@ sub create_related {
 
     my ($from, $to) = %{$meta->map};
 
-    my @params = ($to => $row->column($from));
+    my @where = ($to => $row->column($from));
 
-    if ($meta->class->find(first => 1, where => \@params)) {
+    if ($meta->class->find(first => 1, where => \@where)) {
         Carp::croak('Related object is already created');
     }
 
-    my @objects;
-    foreach my $related (@related) {
-        if (!Scalar::Util::blessed($related)) {
-            $related = $meta->class->new(%$related);
-        }
-        $related->set_columns(@params);
-        $related->create_or_update;
-        push @objects, $related;
+    my $related = $related[0];
+    if (!Scalar::Util::blessed($related)) {
+        $related = $meta->class->new(%$related);
     }
-
-    return @related == 1 ? $objects[0] : @objects;
+    $related->set_columns(@where);
+    return $related->create_or_update;
 }
 
 sub update_related {
-    my $self = shift;
-    my ($row) = shift;
-
+    my $self   = shift;
+    my ($row)  = shift;
     my %params = @_ == 1 ? %{$_[0]} : @_;
 
     my $meta = $self->meta;
 
-    my $rel_table = $meta->class->meta->table;
-
     my ($from, $to) = %{$meta->map};
-    my $where = ["$rel_table.$to" => $row->get_column($from)];
+    my $where = [$to => $row->get_column($from)];
 
-    push @$where, @{$params{where}} if $params{where};
-
-    return $meta->class->table->update(where => $where, @_);
+    return $meta->class->table->update(set => $params{set});
 }
 
 sub delete_related {
     my $self = shift;
     my ($row) = shift;
 
-    my %params = @_;
-    $params{where} ||= [];
-
     my $meta = $self->meta;
 
     my ($from, $to) = %{$meta->map};
+    my $where = [$to => $row->get_column($from)];
 
-    push @{$params{where}}, ($to => $row->get_column($from));
-
-    return $meta->class->table->delete(%params);
+    return $meta->class->table->delete();
 }
 
 1;
