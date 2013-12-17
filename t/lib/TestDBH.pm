748138c8b8a3e8473ddec845798dd2438aeef35a
vti <viacheslav.t@gmail.com>
2013-12-17 15:10:44 +0200

    do not run tests that require specific database driver

--- t/lib/TestDBH.pm
+++ t/lib/TestDBH.pm
@@ -7,6 +7,27 @@ use DBI;
 
 our $DBH;
 
+sub import {
+    my $has_sqlite = eval { require DBD::SQLite; 1 };
+    my $has_mysql  = eval { require DBD::mysql;  1 };
+    my $has_pg     = eval { require DBD::Pg;     1 };
+
+    my $skip_reason;
+    if ($has_sqlite) {
+    } elsif ($has_mysql || $has_pg) {
+        if (!$ENV{TEST_OBJECTDB_DBH}) {
+            $skip_reason = 'Setup TEST_OBJECT_DBH to point to test database';
+        }
+    } else {
+        $skip_reason = 'One of DBD::SQLite, DBD::mysql, DBD::Pg is required';
+    }
+
+    if ($skip_reason) {
+        require Test::More;
+        Test::More->import( skip_all => $skip_reason);
+    }
+}
+
 sub dbh {
     my $class = shift;
 
